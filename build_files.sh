197adfcb5441b869c64844769dfc525bccc49c42
balachandar295 <balachandar295@gmail.com>
2026-04-26 11:07:46 +0530

    Fix Vercel build file to use python3.9

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-pip install -r requirements.txt
-python manage.py collectstatic --noinput
-python manage.py migrate
+python3.9 -m pip install -r requirements.txt
+python3.9 manage.py collectstatic --noinput
+python3.9 manage.py migrate
