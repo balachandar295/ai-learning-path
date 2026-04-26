18cf170f60cc37b5167237b837397089f2cd0323
balachandar295 <balachandar295@gmail.com>
2026-04-26 11:11:15 +0530

    Use virtual environment in build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-python3.9 -m pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput
-python3.9 manage.py migrate
+python3.9 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+python manage.py collectstatic --noinput
+python manage.py migrate
